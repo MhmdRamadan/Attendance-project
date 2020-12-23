a2124b8ec7db92296cefead7f3c9343e5cb9c9eb
mhmd rmadan <mhmdrmadan10@outlook.com>
2020-12-23 10:10:01 +0200

    Add project files.

---
+++ Unit_Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Unit_Testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Unit_Testing")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f82c4006-6c2f-4106-865b-732fa44dfc09")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Unit_Testing/UnitTest1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ATMS_TestingSubject.Controllers;
+using ATMS_TestingSubject.Models;
+using ATMS_TestingSubject;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ATMS_TestingSubject.Classes;
+
+namespace Atm_tests
+{
+    [TestClass]
+    public class UserInfoTest
+    {
+        [TestMethod]
+        public void Login_Test_get()
+        {
+            var controller = new HomeController();
+            var result = controller.Login() as ViewResult;
+            Assert.AreEqual("Login", result.ViewName);
+        }
+        [TestMethod]
+        public void Login_Test_post()
+        {
+            var controller = new HomeController();
+            UserLogin user = new UserLogin() {  Type = "Admin", Passward = "123456", Email = "mhmd@yahoo.com"};
+
+            var result = controller.Login(user) as ViewResult;
+            Assert.AreEqual("Login", result.ViewName);
+        }
+        [TestMethod]
+        public void AdminDashboard_get()
+        {
+            var controller = new AdminController();
+            var result = controller.Index() as ViewResult;
+            Assert.AreEqual("Index", result.ViewName);
+        }
+        [TestMethod]
+        public void Details_get()
+        {
+            var controller = new AdminController();
+            var result = controller.DetailsMe() as ViewResult;
+            Assert.AreEqual("DetailsMe", result.ViewName);
+        }
+        [TestMethod]
+        public void Create_get()
+        {
+
+            var controller = new HomeController();
+            var result = controller.Register() as ViewResult;
+            Assert.AreEqual("Register", result.ViewName);
+        }
+
+        [TestMethod]
+        public void Create_post()
+        {
+            ATMS_Model repository = new ATMS_Model();
+            var controller = new UserInfoController();
+            UserInfo user = new UserInfo();
+            repository.SaveChanges();
+            List<UserInfo> users = repository.UserInfoes.ToList();
+            Assert.IsFalse(users.Contains(user));
+            // Assert.AreEqual("Create", result.ViewName);
+        }
+        [TestMethod]
+        public void Edit_get()
+        {
+            var controller = new AdminController();
+            var result = controller.EditMe() as ViewResult;
+            Assert.AreEqual("EditMe", result.ViewName);
+        }
+        [TestMethod]
+        public void Edit_post()
+        {
+            var controller = new AdminController();
+            UserInfo user = new UserInfo() { Name = "Mohamed", Type = "Admin", Passward = "123456", Email = "mhmd@yahoo.com", Gender = "Male" };
+            var result = controller.EditMe(user) as ViewResult;
+                Assert.AreEqual("EditMe", result.ViewName);
+        }
+
+
+    }
+}
